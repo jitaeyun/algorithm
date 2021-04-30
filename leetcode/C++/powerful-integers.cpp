4ad83f6fcd7ed1ad2022b29fc74c18394ab8ef60
jitaeyun <oneyjt@gmail.com>
2021-04-30 21:49:27 +0900

    [mod] Powerful Integers

--- leetcode/C++/powerful-integers.cpp
+++ leetcode/C++/powerful-integers.cpp
@@ -2,26 +2,14 @@ class Solution {
 public:
     vector<int> powerfulIntegers(int x, int y, int bound) {
         vector<int> sol,m(bound+1,0);
-        int sz;
-        for(int i=1; i+1<=bound; i*=x) {
-            m[i+1]=1;
-            sol.push_back(i+1);
-            if(x==1) break;
-        }
-        sz=sol.size();
-        for(int i=y,t=0; i<=bound; i*=y){
-            for(int j=0; j<sz; ++j){
-                t = i + sol[j] -1;
-                if(t<=bound){
-                    if(m[t]==0){
-                        m[t]=1;
-                        sol.push_back(t);
-                    }
-                }
-                else if(i==0) return sol;
-                else break;
+        int lx=1, ly=1, n;
+        if(x!=1) lx=int(floor(log(bound) / log(x))) + 1;
+        if(y!=1) ly=int(floor(log(bound) / log(y))) + 1;
+        for(int i=0, px=1; i<lx; ++i, px*=x){
+            for(int j=0, py=1; j<ly; ++j, py*=y){
+                n=px+py;
+                if(n<=bound&&m[n]++==0) sol.push_back(n);
             }
-            if(y==1) break;
         }
         return sol;
     }
