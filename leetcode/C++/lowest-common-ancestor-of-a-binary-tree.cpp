c01dfaa4eeff54a212e8e26627029e247b2cac28
jitaeyun <oneyjt@gmail.com>
2021-06-30 20:43:25 +0900

    [mod] Lowest Common Ancestor of a Binary Tree

--- leetcode/C++/lowest-common-ancestor-of-a-binary-tree.cpp
+++ leetcode/C++/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -32,3 +32,20 @@ public:
         return l.back();
     }
 };
+
+
+//another solution
+/*
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==NULL) return root;
+        if(root->val==p->val || root->val==q->val) return root;
+        TreeNode* l = lowestCommonAncestor(root->left,p,q);
+        TreeNode* r = lowestCommonAncestor(root->right,p,q);
+        if(l==NULL) return r;
+        else if(r==NULL) return l;
+        return root;
+    }
+};
+*/
--- leetcode/Python/lowest-common-ancestor-of-a-binary-tree.py
+++ leetcode/Python/lowest-common-ancestor-of-a-binary-tree.py
@@ -31,3 +31,27 @@ class Solution:
             l.pop()
             r.pop()
         return l[-1]
+
+'''
+# anosolution
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root is None:
+            return root
+        if root.val == p.val or root.val == q.val:
+            return root
+        l = self.lowestCommonAncestor(root.left,p,q)
+        r = self.lowestCommonAncestor(root.right,p,q)
+        if l is None:
+            return r
+        elif r is None:
+            return l
+        return root
+'''
