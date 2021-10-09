976614b6f4604ed1f1bf494baea9fcf6c0af03a8
jitaeyun <oneyjt@gmail.com>
2021-10-09 16:48:38 +0900

    [mod] Update fast code in problem 0212

--- leetcode/C++/word-search-ii.cpp
+++ leetcode/C++/word-search-ii.cpp
@@ -1,51 +1,49 @@
 class NODE {
 public:
     vector<NODE*> v;
-    NODE(): v(26,nullptr) {}
+    int fin;
+    NODE(): v(26,nullptr) {fin=0;}
 };
 
 class Solution {
 public:
+    vector<string> sol;
     int mx,my,dy[4]={0,0,1,-1},dx[4]={1,-1,0,0};
     NODE *node;
-    void traverse(int &y,int &x, int idx,vector<vector<char>>& board, NODE *cur){
+    void make_path(string &word, int &idx){
+        NODE *cur = node;
+        for(char &ch : word){
+            if(cur->v[ch-'a']==nullptr) cur->v[ch-'a']=new NODE();
+            cur=cur->v[ch-'a'];
+        }
+        cur->fin = idx+1;
+    }
+    void traverse(int &y,int &x, int idx, vector<vector<char>>& board, vector<string> &words, NODE *cur){
         int ny=0,nx=0;
-        if(idx==10) return;
-        if(cur->v[board[y][x]-'a']==nullptr) cur->v[board[y][x]-'a']=new NODE();
-        cur = cur->v[board[y][x]-'a'];
+        if(idx==10 || (cur=cur->v[board[y][x]-'a'])==nullptr) return;
+        else if(cur->fin){
+            sol.push_back(words[cur->fin-1]);
+            cur->fin = 0;
+        }
         board[y][x]=-board[y][x];
         for(int i=0; i<4; ++i){
             ny=y+dy[i]; nx=x+dx[i];
             if(ny<0 || ny>=my || nx<0 || nx>=mx || board[ny][nx]<0) continue;
-            traverse(ny,nx,idx+1,board,cur);
+            traverse(ny,nx,idx+1,board,words,cur);
         }
         board[y][x]=-board[y][x];
-    }
-    bool search_prefix(string &word){
-        NODE *cur = node;
-        for(char &ch : word){
-            if(cur->v[ch-'a']==nullptr) return false;
-            else cur=cur->v[ch-'a'];
-        }
-        return true;
     }
 
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
         // 1. init
-        vector<string> sol;
-        unordered_set<char> us;
-        my=board.size(); mx=board[0].size();
-        node = new NODE();
-        for (string &word: words) us.insert(word[0]);
-        // 2. traverse
+        my=board.size(); mx=board[0].size(); node = new NODE();
+        // 2. make path
+        for (int i=0; i<words.size(); ++i) make_path(words[i], i);
+        // 3. traverse
         for(int i=0; i<board.size(); ++i){
             for(int j=0; j<board[0].size(); ++j){
-                if(us.find(board[i][j]) != us.end()) traverse(i,j,0,board,node);
+                traverse(i,j,0,board,words,node);
             }
-        }
-        // 3. search prefix
-        for(string &word : words){
-            if(search_prefix(word)) sol.push_back(word);
         }
         return sol;
     }
--- leetcode/Python/word-search-ii.py
+++ leetcode/Python/word-search-ii.py
@@ -1,40 +1,33 @@
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        def _make_path(idx):
+            cur = node
+            for c in words[idx]:
+                cur = cur.setdefault(c, dict())
+            cur['fin'] = idx + 1
         def _traverse(y, x, idx, cur):
-            if idx==10:
+            orig, n = board[y][x], cur.get(board[y][x])
+            if idx==10 or n is None:
                 return
-            orig, n = board[y][x], cur
+            elif 'fin' in n:
+                sol.append(words[n['fin']-1])
+                del n['fin']
             board[y][x] = '0'
-            if cur.get(orig) is None:
-                n = cur[orig] = dict()
-            else:
-                n = cur[orig]
             for i in range(4):
                 ny, nx = y + dy[i], x+ dx[i]
                 if ny<0 or ny>=my or nx<0 or nx>=mx or board[ny][nx]=='0':
                     continue
                 _traverse(ny, nx, idx+1, n)
             board[y][x] = orig
-        def _search_prefix(word):
-            cur = um
-            for c in word:
-                if (cur := cur.get(c)) is None:
-                    return False
-            return True
-                    
         # 1. init
         dy, dx, my, mx = [1,-1,0,0], [0,0,1,-1], len(board), len(board[0])
-        sol, d, um = list(), set(), dict()
-        for word in words:
-            d.add(word[0])
-        # 2. traverse
+        sol, node = list(), dict()
+        # 2. make path
+        for i in range(len(words)):
+            _make_path(i)
+        # 3. traverse
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if board[i][j] in d:
-                    _traverse(i,j,0,um)
-        # 3. search_prefix
-        for word in words:
-            if _search_prefix(word):
-                sol.append(word)
+                _traverse(i,j,0,node)
         return sol
         
