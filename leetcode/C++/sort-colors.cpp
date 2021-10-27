2198c43517b62373c659cd54fe1dbbda64b44d46
jitaeyun <oneyjt@gmail.com>
2021-10-27 23:32:56 +0900

    [mod] Update another solution in p0075

--- leetcode/C++/sort-colors.cpp
+++ leetcode/C++/sort-colors.cpp
@@ -4,3 +4,19 @@ public:
         return stable_sort(nums.begin(),nums.end());
     }
 };
+
+
+/*
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        vector<int> v(3,0);
+        for(int &n : nums) ++v[n];
+        for(int i=0,k=0; i<v.size(); ++i){
+            for(int j=0; j<v[i]; ++j){
+                nums[k++]=i;
+            }
+        }
+    }
+};
+*/
--- leetcode/Python/sort-colors.py
+++ leetcode/Python/sort-colors.py
@@ -2,3 +2,15 @@ class Solution:
     def sortColors(self, nums: List[int]) -> None:
         nums.sort()
         return nums
+
+'''
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        v,idx = [0]*3, 0
+        for n in nums:
+            v[n]+=1
+        for i in range(len(v)):
+            for j in range(v[i]):
+                nums[idx], idx = i, idx+1
+        
+'''
