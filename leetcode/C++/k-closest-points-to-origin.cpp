30a57787c9ea855043c19aa376f2433e3fedc2b5
jitaeyun <oneyjt@gmail.com>
2021-12-26 13:59:30 +0900

    [mod] Update another solution in p0973

--- leetcode/C++/k-closest-points-to-origin.cpp
+++ leetcode/C++/k-closest-points-to-origin.cpp
@@ -11,3 +11,31 @@ public:
         return sol;
     }
 };
+
+
+
+/*
+class POINT {
+public:
+    int val,idx;
+    POINT() {}
+    POINT(int v, int i) : val(v), idx(i) {}
+    bool operator()(POINT &a, POINT &b){
+        return (a.val==b.val)? a.idx>b.idx : a.val>b.val;
+    }
+};
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        vector<vector<int>> sol;
+        priority_queue<POINT,vector<POINT>, POINT> pq;
+        for(int i=0; i<points.size(); ++i) {
+            pq.push(POINT(points[i][0]*points[i][0]+points[i][1]*points[i][1], i));
+        }
+        for(int i=0; i<k; ++i) {
+            sol.push_back(points[pq.top().idx]); pq.pop();
+        }
+        return sol;
+    }
+};
+*/
--- leetcode/Python/k-closest-points-to-origin.py
+++ leetcode/Python/k-closest-points-to-origin.py
@@ -8,3 +8,16 @@ class Solution:
         for i in range(k):
             sol.append(points[v[i][1]])
         return sol
+
+
+'''
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        sol, hq = list(), list()
+        for i in range(len(points)):
+            heapq.heappush(hq, (points[i][0]*points[i][0]+points[i][1]*points[i][1], i))
+        for i in range(k):
+            t = heapq.heappop(hq)
+            sol.append(points[t[1]])
+        return sol
+'''
