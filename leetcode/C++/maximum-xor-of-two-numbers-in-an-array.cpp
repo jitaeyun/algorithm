43dfc49d6b8b66329c30b2d3354783a5207f5a6e
jitaeyun <oneyjt@gmail.com>
2022-01-27 22:46:06 +0900

    [mod] Update performance enhancement in p0421

--- leetcode/C++/maximum-xor-of-two-numbers-in-an-array.cpp
+++ leetcode/C++/maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,17 +1,16 @@
 class TRIE {
 public:
-    vector<int> v;
+    int count, val;
     TRIE* n;
-    TRIE(){n = nullptr;}
+    TRIE(){n = nullptr; count=val=0;}
     void makeTrie(){n = new TRIE[2];}
-    void insertNode(int &max_bit, int &num, int &idx){
+    void insertNode(int &max_bit, int &num){
         TRIE *r = this;
-        r->v.push_back(idx);
          for(int i=max_bit,bit=0; i>0; i>>=1){
             bit = (i&num)? 1 : 0; 
             if(r->n==nullptr) r->makeTrie();
             r = &(r->n[bit]);
-            r->v.push_back(idx);
+            ++(r->count); r->val = num;
         }
     }
 };
@@ -28,16 +27,16 @@ public:
         TRIE *t = new TRIE(), *r = nullptr;
         for(int &n : nums) max_bit = max(max_bit, n);
         max_bit = find_max_bit(max_bit);
-        for(int i=0; i<nums.size(); ++i) t->insertNode(max_bit, nums[i], i);
+        for(int i=0; i<nums.size(); ++i) t->insertNode(max_bit, nums[i]);
         for(int i=0,idx=0; i<nums.size(); ++i){
             if(nums[i]<max_bit) continue;
             r = t;
             for(int j=max_bit; j>0; j>>=1){
                 idx = (j&nums[i])? 0 : 1;
-                if(!r->n[idx].v.empty()) r=&(r->n[idx]);
+                if(r->n[idx].count > 0) r=&(r->n[idx]);
                 else r=&(r->n[idx^1]);
             }
-            sol = max(sol, nums[r->v[0]]^nums[i]);
+            sol = max(sol, r->val^nums[i]);
         }
         return sol;
     }
--- leetcode/Python/maximum-xor-of-two-numbers-in-an-array.py
+++ leetcode/Python/maximum-xor-of-two-numbers-in-an-array.py
@@ -1,43 +1,37 @@
-class TRIE:
-    def __init__(self):
-        self.n = None
-        self.v = list()
-    def makeTrie(self):
-        self.n = [TRIE(), TRIE()]
-    def insertNode(self, bit, num, idx):
-        r = self
-        r.v.append(idx)
-        while bit > 0:
-            i = 1 if (bit&num)>0 else 0
-            if r.n is None:
-                r.makeTrie()
-            r = r.n[i]
-            r.v.append(idx)
-            bit >>= 1
 class Solution:
     def findMaximumXOR(self, nums: List[int]) -> int:
+        def initTrie():
+            return {'next': None, 'count':0, 'val': 0}
+        def makeTrie(trie, bit, num):
+            t = trie;
+            while bit > 0:
+                i = 1 if (bit&num)>0 else 0
+                if t.get('next', None) is None:
+                    t['next'] = [initTrie(), initTrie()]
+                t = t.get('next')[i]
+                t['val'], t['count'] = num, t['count'] + 1
+                bit >>= 1
         def find_max_bit(num):
             bit = 1<<30
             while bit > 0:
                 if (bit&num) > 0:
                     return bit
                 bit >>= 1
-            return 0
-        sol, max_bit = 0, max(nums)
-        t = TRIE()
+            return 1
+        sol, max_bit, t = 0, max(nums), initTrie()
         max_bit = find_max_bit(max_bit)
-        for i in range(len(nums)):
-            t.insertNode(max_bit, nums[i], i)
-        for i in range(len(nums)):
-            if nums[i] < max_bit:
+        for num in nums:
+            makeTrie(t, max_bit, num)
+        for num in nums:
+            if num < max_bit:
                 continue
             r, bit = t, max_bit
             while bit > 0:
-                idx = 0 if (bit&nums[i])>0 else 1
-                if len(r.n[idx].v) == 0:
-                    r = r.n[idx^1]
+                idx = 0 if (bit&num)>0 else 1
+                if r['next'][idx]['count'] == 0:
+                    r = r['next'][idx^1]
                 else:
-                    r = r.n[idx]
+                    r = r['next'][idx]
                 bit >>= 1
-            sol = max(sol, nums[r.v[0]]^nums[i])
+            sol = max(sol, r.get('val')^num)
         return sol
