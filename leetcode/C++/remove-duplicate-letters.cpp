b9080be04321d893ca1c6a7f6908eb7e04fae1a7
jitaeyun <oneyjt@gmail.com>
2022-03-18 22:43:53 +0900

    [mod] add another solution in P316

--- leetcode/C++/remove-duplicate-letters.cpp
+++ leetcode/C++/remove-duplicate-letters.cpp
@@ -23,3 +23,56 @@ public:
         return sol;
     }
 };
+
+
+/*
+//using stack
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        string sol;
+        vector<int> visit(26,0), v(26,0);
+        int idx;
+        for(char &ch : s) ++v[ch-'a'];
+        for(char &ch : s){
+            idx = ch-'a';
+            --v[idx];
+            if(visit[idx]) continue;
+            for(; sol.length()>0 && sol.back() > ch && v[sol.back()-'a']>0; sol.pop_back()) visit[sol.back()-'a']=0;
+            sol.push_back(ch);
+            visit[idx]=1;
+        }
+        return sol;
+    }
+};
+*/
+
+
+
+/*
+// using bitmask
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        string sol;
+        int visit=0;
+        vector<int> v(s.length(),0);
+        for(int i=(int)s.length()-1; i>=0; --i) {
+            visit|=(1<<(s[i]-'a'));
+            v[i]=visit;
+        }
+        for(int i=0, idx=0,max_idx=-1; i<26 && visit>0; ++i){
+            max_idx=-1;
+            for(int j=idx; j<s.length() && (v[j]&visit)==visit; ++j){
+                if(((1<<(s[j]-'a'))&visit)==0) continue;
+                else if(max_idx==-1) max_idx=j;
+                else if(s[max_idx]>s[j]) max_idx=j;
+            }
+            sol.push_back(s[max_idx]);
+            visit^=(1<<(s[max_idx]-'a'));
+            idx=max_idx+1;
+        }
+        return sol;
+    }
+};
+*/
